6b9a7299a9f594ccaf652b24c1956f2e8231b04b
Tomasz Cichoń <tomasz.cichon@itsilesia.com>
2022-10-29 13:41:28 +0200

    App dev start

---
+++ ACSupport/src/main/java/pl/polsl/acsupport/AcSupportApplication.java
@@ -0,0 +1,13 @@
+package pl.polsl.acsupport;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AcSupportApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AcSupportApplication.class, args);
+	}
+
+}
