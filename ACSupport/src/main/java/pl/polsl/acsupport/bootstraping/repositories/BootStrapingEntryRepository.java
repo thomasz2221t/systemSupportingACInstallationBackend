a36450300d7c973e620ee0761507a996406df726
Tomasz Cichoń <tomekcichon@onet.pl>
2022-11-11 01:56:02 +0100

    Optmised unused imports

--- ACSupport/src/main/java/pl/polsl/acsupport/bootstraping/repositories/BootStrapingEntryRepository.java
+++ ACSupport/src/main/java/pl/polsl/acsupport/bootstraping/repositories/BootStrapingEntryRepository.java
@@ -2,8 +2,8 @@ package pl.polsl.acsupport.bootstraping.repositories;
 
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
-import pl.polsl.acsupport.entities.BootStrapingEntry;
 import pl.polsl.acsupport.bootstraping.enums.BootStrapingLabel;
+import pl.polsl.acsupport.entities.BootStrapingEntry;
 
 import java.util.Optional;
 
