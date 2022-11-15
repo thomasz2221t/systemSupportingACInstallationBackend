a82fff30eeedd7d7410bb9d4bf334fe214ed768f
Tomasz Cichoń <tomasz.cichon@itsilesia.com>
2022-11-15 01:39:52 +0100

    Finished login page

--- acsupportfront/src/components/LoginWindow.tsx
+++ acsupportfront/src/components/LoginWindow.tsx
@@ -1,12 +1,47 @@
 import React from "react";
+import { Icon } from "@iconify/react";
+import "./LoginWindow.scss";
 const LoginWindow = () => {
   return (
     <>
       <div className="login-panel">
-        <div className="login-input"></div>
-        <div className="password-input"></div>
-        <div className="log-in-button"></div>
-        <div className="create-acount-link"></div>
+        <div className="log-in-label">
+          <p>Zaloguj się</p>
+        </div>
+        <form>
+          <div>
+            <Icon
+              className="login-icon"
+              icon="mdi:user-circle-outline"
+              color="#4e4e4e"
+              height="32"
+              //onClick={() => setOpenParameterTypeForm(true)
+            />
+            <input className="login-input" type="text" placeholder="login" />
+          </div>
+          <div>
+            <Icon
+              className="password-icon"
+              icon="material-symbols:key-outline-rounded"
+              color="#4e4e4e"
+              height="32"
+              //onClick={() => setOpenParameterTypeForm(true)
+            />
+            <input
+              className="password-input"
+              type="password"
+              placeholder="hasło"
+            />
+          </div>
+          <div>
+            <button className="log-in-button" type="submit">
+              Zaloguj
+            </button>
+          </div>
+          <div className="create-account-link">
+            <p>Nie masz konta? Zarejestruj się</p>
+          </div>
+        </form>
       </div>
     </>
   );
