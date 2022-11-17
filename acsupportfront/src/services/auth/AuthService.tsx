2b9e03a24bb5428cfada90789ba894d0274f817f
Tomasz Cichoń <tomasz.cichon@itsilesia.com>
2022-11-17 23:52:44 +0100

    Added AuthService

---
+++ acsupportfront/src/services/auth/AuthService.tsx
@@ -0,0 +1,41 @@
+import axios from "axios";
+import { UserType } from "./UserService";
+
+const API_URL = "http://localhost:8080";
+
+const login = (login: string, password: string) => {
+  return axios
+    .post(`${API_URL}/login`, {
+      login,
+      password,
+    })
+    .then((response) => {
+      if (response.data.accessToken) {
+        localStorage.setItem("user", JSON.stringify(response.data));
+      }
+      return response.data;
+    });
+};
+
+const logout = () => {
+  localStorage.removeItem("user");
+};
+
+const register = (props: UserType) => {
+  return axios.post(`${API_URL}`, {
+    props,
+  });
+};
+
+const getCurrentUser = () => {
+  return JSON.parse(localStorage.getItem("user")!);
+};
+
+const AuthService = {
+  login,
+  logout,
+  register,
+  getCurrentUser,
+};
+
+export default AuthService;
