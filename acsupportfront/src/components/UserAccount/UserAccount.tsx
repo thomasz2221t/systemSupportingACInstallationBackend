277bb2b9db2cfe17843455ad63d9cc3139bd957f
Tomasz Cichoń <tomasz.cichon@itsilesia.com>
2022-11-21 01:30:13 +0100

    Added UserAccount to ObjectPage

---
+++ acsupportfront/src/components/UserAccount/UserAccount.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { Select } from "@mui/material";
+
+export default function UserAccount() {
+  return (
+    <>
+      <div>
+        <Select></Select>
+      </div>
+    </>
+  );
+}
