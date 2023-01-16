01a3f25881a79211143f7826605db239eb2fde38
Tomasz Cichoń <tomekcichon@onet.pl>
2023-01-16 03:26:27 +0100

    Added minor fix to security

--- ACSupport/src/main/java/pl/polsl/acsupport/config/SecurityConfig.java
+++ ACSupport/src/main/java/pl/polsl/acsupport/config/SecurityConfig.java
@@ -49,7 +49,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http.authorizeRequests()
-                .antMatchers( "/user/**")
+                .antMatchers( "/user/**", "/login/**")
                 .permitAll()
                 .anyRequest().authenticated()
                 .and()
